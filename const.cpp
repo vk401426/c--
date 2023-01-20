765c63e2862d03d41b06e7060ed459c1b5904688
vk401426 <vk401426@gmail.com>
2023-01-20 05:46:09 +0530

    hi

---
+++ const.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ class date
+ {
+    public :
+                date()
+                {
+                    cout<<endl<<"Date 70";
+                    cout<<endl<<"Month january";
+                    cout<<endl<<"year 2023";
+                }
+ };
+ int main()
+ {
+    date today;
+    return 0;
+ }
